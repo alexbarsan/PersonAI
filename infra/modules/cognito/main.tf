7ff2ad1f23cbbb00051f0c8f9cc035fcb2ecb1b7
Alex Bar <alex.barsan20@gmail.com>
2026-09-11 01:13:46 +0300

    feat: reduce Cognito password minimum to six

--- infra/modules/cognito/main.tf
+++ infra/modules/cognito/main.tf
@@ -5,7 +5,7 @@ resource "aws_cognito_user_pool" "this" {
   mfa_configuration        = "OPTIONAL"
 
   password_policy {
-    minimum_length                   = 12
+    minimum_length                   = 6
     require_lowercase                = true
     require_numbers                  = true
     require_symbols                  = true
